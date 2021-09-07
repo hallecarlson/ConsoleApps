db01dd02f63365291c9704226bdee72424d33ddd
Halle Carlson <carlsonh@rsd407.org>
2021-09-07 08:45:28 -0700

    added a new line of output

--- ConsoleApps/ConsoleApps.cpp
+++ ConsoleApps/ConsoleApps.cpp
@@ -1,11 +1,14 @@
 // ConsoleApps.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <stdio.h>
+#include <conio.h>
 #include <iostream>
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n";
+    printf("Good morning\n");
+    return 0;
 }
-
 
