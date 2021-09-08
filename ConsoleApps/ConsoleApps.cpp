15510316ff67d3c2af0d8c0dc879662577cfc7b5
Halle Carlson <carlsonh@rsd407.org>
2021-09-08 08:39:58 -0700

    added 3 new lines of output

--- ConsoleApps/ConsoleApps.cpp
+++ ConsoleApps/ConsoleApps.cpp
@@ -8,7 +8,11 @@ using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
-    printf("Good morning\n");
+    printf("Good morning!\n");
+    printf("The skies are clear.\n");
+    printf("The sun is bright.\n");
+    printf("The air is cool.\n");
+    printf("It's a nice day for a walk.\n");
     return 0;
 }
 
