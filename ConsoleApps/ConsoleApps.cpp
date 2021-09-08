c2eaf12376ae0364e057ec67e3db886d06259be4
Halle Carlson <carlsonh@rsd407.org>
2021-09-08 08:49:45 -0700

    updated pseudocode

--- ConsoleApps/ConsoleApps.cpp
+++ ConsoleApps/ConsoleApps.cpp
@@ -1,5 +1,16 @@
 // ConsoleApps.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* Author: Halle Carlson
+   Project Name: Hello World
+   File Name: ConsoleApps.cpp
+   Purpose: Output Greeting
+   Pseudocode: English explanation of code
+   Maintenance Log:
+   began project 9/7/2021
+   added #include <stdio.h>, #include <conio.h>, using namespace std;, on new line to output 9/7/2021
+   purposefully created an error in code 9/7/2021
+   fixed error in code 9/7/2021
+   added 3 new lines to output 9/8/2021
+   */
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
