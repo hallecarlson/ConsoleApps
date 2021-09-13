b00af7f7a2b5f2164c7c41cd750488274fa3bbfe
Halle Carlson <carlsonh@rsd407.org>
2021-09-13 08:32:34 -0700

    Started project and added pseudocode.

---
+++ VariablesAndConstants/VariablesAndConstants.cpp
@@ -0,0 +1,27 @@
+// VariablesAndConstants.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/* Author: Halle Carlson
+   Project Name: Variables and Constants
+   File Name: VariablesAndConstants.cpp
+   Purpose: Output variables and constants
+   Pseudocode: English explanation of code
+   Maintenance Log:
+   Began project 9/13/21
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
