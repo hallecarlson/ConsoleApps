472a23de4c4e3a604376ae6e6ac022220fcb3429
Halle Carlson <carlsonh@rsd407.org>
2021-09-15 07:59:46 -0700

    Added new lines of code that declare, initialize, and output variables and constants

--- VariablesAndConstants/VariablesAndConstants.cpp
+++ VariablesAndConstants/VariablesAndConstants.cpp
@@ -22,25 +22,43 @@
 
 int main()
 {
-    printf("Hello World!\n");    // first output to the screen
-    int a = 0;
-    const float b = 10.0;
-    char d = 'q';               // CHANGE THIS CONSTANT to something from math or science
-    const float c = 3.0e8;      // speed of light in m/s
-    const float g = 9.8;        // acceleration due to gravity in m/s^2
-    printf("a = %i\n", a); 
-    printf("b = %f\n", b);
+    printf("Hello World!\n");       // first output to the screen
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    char d = 'q';
+    char f = 'r';
+    char h = 's';
+    const float e = 2.7182818;      // Euler's number (e)        
+    const float c = 3.0e8;          // speed of light in m/s
+    const float g = 9.8;            // acceleration due to gravity in m/s^2
+    const float a = 6.0221515e23;   // Avogadro's Constant
+    const float b = 1.380650e23;    // Boltzmann's Constant
+    printf("x = %i\n", x);
     printf("d = %c\n", d);
+    printf("e = %f\n", e);
     printf("c = %f\n", c);
     printf("g = %f\n", g);
+    printf("a = %f\n", a);
+    printf("b = %f\n", b);
 
-    a = 14;
+    x = 8;
+    y = 16;
+    z = 32;
     d = 'x';
-    printf("a = %i\n", a);
-    printf("b = %f\n", b);
+    f = 'y';
+    h = 'z';
+    printf("x = %i\n", x);
+    printf("y = %i\n", y);
+    printf("z = %i\n", z);
     printf("d = %c\n", d);
+    printf("f = %c\n", f);
+    printf("h = %c\n", h);
+    printf("e = %f\n", e);
     printf("c = %f\n", c);
     printf("g = %f\n", g);
+    printf("a = %f\n", a);
+    printf("b = %f\n", b);
 
     printf("Press any key to continue.\n");
     _getch();
