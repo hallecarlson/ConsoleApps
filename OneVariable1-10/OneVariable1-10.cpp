19b8ec661d14d23212b2d68ebec65fdabf73dd10
Halle Carlson <carlsonh@rsd407.org>
2021-09-24 08:10:21 -0700

    Added code, psuedocode, and comments

--- OneVariable1-10/OneVariable1-10.cpp
+++ OneVariable1-10/OneVariable1-10.cpp
@@ -1,5 +1,21 @@
 // OneVariable1-10.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+    Author: Halle Carlson
+    Problem:
+    Project Name: 
+    File Name:
+    Pseudocode:
+    Maintenance Log:
+    Date: 9/22/21   Done:
+                    Began project
+                    Added code to print numbers 1-10 with one int variable
+                    Pushed to GitHub
+
+    Date: 9/24/21   Done:
+                    Added code to print numbers 1-10 with one int variable
+                    Added pseudocode and comments
+                    Pushed to GitHub
+*/
 
 #include <iostream>
 #include <stdio.h>
@@ -10,6 +26,16 @@ int main()
 {
     //std::cout << "Hello World!\n";
     char c = '1',
+    printf("%c\n", c);
+    c++;
+    printf("%c\n", c);
+    c++;
+    printf("%c\n", c);
+    c++;
+    printf("%c\n", c);
+    c++;
+    printf("%c\n", c);
+    c++;
     printf("%c\n", c);
     c++;
     printf("%c\n", c);
