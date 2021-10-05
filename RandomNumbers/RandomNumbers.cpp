382fb05e644c3261bcd6422656059c0ff65fe627
Halle Carlson <carlsonh@rsd407.org>
2021-10-05 08:45:21 -0700

    Added and updated code, comments, and pseudocode

--- RandomNumbers/RandomNumbers.cpp
+++ RandomNumbers/RandomNumbers.cpp
@@ -1,12 +1,16 @@
 // RandomNumbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
 /*
-    Author: Halle Carlson
-    Problem: Generate random numbers by simulating a 6-sided die and a 12-sided die, as well as generating a 
-    random float between 00 and 5.0 (inclusive)
-    Project name:
-    File name:
+    Author:         Halle Carlson
+    Problem:        Generate random numbers by simulating a 6-sided die and a 12-sided die, as well as generating a 
+                    random float between 00 and 5.0 (inclusive) and a random capital letter.
+    Project name:   Problem GS02-03 (dice and other random numbers)
+    File name:      RandomNumbers.cpp
 
-    Pseudocode:
+    Pseudocode:     The program generates a series of random numbers. Using int variables, the program simulates a  
+                    6-sided die and a 12-sided die. Using a float variable, the program generates a random number  
+                    between 0.0 and 5.0, inclusive of 5.0. Using a char variable and ASCII codes, the program generates     
+                    a random capital letter. Each output is labeled, with a number of spaces following the label in 
+                    the code so that each random number output lines up.
 
     Maintenance Log:
     Date: 10/4/21   Done:
@@ -14,9 +18,19 @@
                     Added comments
                     Added code to simulate 6-sided die and 12-sided die
                     Fixed errors
-                    Added code to generate f=random float 
+                    Added code to generate random float between 0.0 and 5.0
                     Fixed Errors
                     Updated comments
+                    Pushed to GitHub
+
+    Date: 10/5/21   Done:
+                    Updated code to generate random float between 0.0 and 5.0
+                    Added code to generate random capital letter using ASCII codes
+                    Fixed errors
+                    Renamed variables for organization purposes
+                    Organized code to be easier for programmer to read
+                    Formatted output
+                    Updated comments and pseudocode
                     Pushed to GitHub
 */
 
@@ -31,19 +45,26 @@ int main()
 {
     //srand(0);
     srand((unsigned)time(NULL));
-    int die_min;
-    int die_max;
-    int die_rn;
-    float float_min;
-    float float_max;
-    float float_rn;
-    die_min = 1;                                  //6-sided die
-    die_max = 6;
-    die_rn = die_min + rand() % (die_max - die_min + 1);
-    printf("6-sided die roll = % i\n", die_rn);
-    die_max = 12;
-    die_rn = die_min + rand() % (die_max - die_min + 1);
-    printf("12-sided die roll = % i\n", die_rn);
+    
+    int min_die = 1;                                    
+    int max_die = 6;
+    int rn = min_die + rand() % (max_die - min_die + 1);    //[1, 6] 6-sided die
+    printf("6-sided die roll      ");                       //space to format output
+    printf("= %i\n", rn);
+    max_die = 12;                                       
+    rn = min_die + rand() % (max_die - min_die + 1);        //[1, 12] 12-sided die
+    printf("12-sided die roll     ");                       //space to format output
+    printf("= %i\n", rn);
+    
+    float rf = (float)rand() / (float)RAND_MAX * 5.0;       //[0.0, 5.0] random float within range
+    printf("Random float          ");                       //space to format output
+    printf("= %f\n", rf);
+
+    char min_c = 65;                                        //65 == A (ASCII code)
+    char max_c = 90;                                        //90 == Z (ASCII code)
+    char rc;
+    rc = min_c + rand() % (max_c - min_c + 1);              //[A, Z] random capital letter
+    printf("Random capital letter = %c\n", rc);
 }
 
 
---
+++ Test/Test.cpp
@@ -0,0 +1,20 @@
+// Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a;
+    a = 4;
+    switch (a)
+    {
+        case 1:
+            printf("one\n");
+            break;
+        case 2:
+            printf("two\n");
+            break;
+    }
+}
+
