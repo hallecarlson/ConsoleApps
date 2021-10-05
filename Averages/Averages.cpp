a3046708a86d04c73d9c8449f89f1c7df9f79692
Halle Carlson <carlsonh@rsd407.org>
2021-10-05 08:58:03 -0700

    Created two new projects and added comments

---
+++ Averages/Averages.cpp
@@ -0,0 +1,21 @@
+// Averages.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+    Author: Halle Carlson
+    Problem:
+    Project name: GS02-06 (Averages)
+    File name: Averages.cpp
+
+    Pseudocode:
+
+    Maintenance log:
+    Date:           Done:
+
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ BusCharter/BusCharter.cpp
@@ -0,0 +1,21 @@
+// BusCharter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+    Author: Halle Carlson
+    Problem: 
+    Project name: GS02-05 (Bus Charter)
+    File name: BusCharter.cpp
+
+    Pseudocode:
+
+    Maintenance log:
+    Date:           Done:
+
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
