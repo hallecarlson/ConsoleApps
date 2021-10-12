60a6d623dc9a1ccd2bc0c3983bec055fa4e49599
Halle Carlson <carlsonh@rsd407.org>
2021-10-12 08:55:18 -0700

    Tested code for pair programming - more random stuff

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,19 +2,18 @@
 //
 
 #include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
 
 int main()
 {
-    int a;
-    a = 4;
-    switch (a)
-    {
-        case 1:
-            printf("one\n");
-            break;
-        case 2:
-            printf("two\n");
-            break;
-    }
+	srand((unsigned)time(NULL));
+
+	float rf = (float)rand() / (float)RAND_MAX;				//[0.0, 5.0] random float within range
+	printf("Random float          ");                       //space to format output
+	printf("= %f\n", rf);
 }
 
