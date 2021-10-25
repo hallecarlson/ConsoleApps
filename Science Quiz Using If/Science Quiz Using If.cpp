ee11afd863eccc1c06e42ff7908dfaa5103dd27c
Halle Carlson <carlsonh@rsd407.org>
2021-10-25 08:33:41 -0700

    Added comments, added code for variables, player name input, and text output, formatted code and comments

--- Science Quiz Using If/Science Quiz Using If.cpp
+++ Science Quiz Using If/Science Quiz Using If.cpp
@@ -1,24 +1,86 @@
 // Science Quiz Using If.cpp : This file contains the 'main' function. Program execution begins and ends there.
 /*
+    Author:         Halle Carlson
+    Project Name:   Problem GS03-02 (Science Quiz using if)
+    File Name:      Science Quiz Using If.cpp
+    Purpose:        
 
-Maintenance Log:
-Date: 10/22/21  Done: Created project
+    Pseudocode:
+
+    Maintenance Log:
+    Date: 10/22/21  Done: 
+                    Created project
+
+    Date: 10/25/21  Done:
+                    Added comments
+                    Added code for int and string variables, player name input, and text output
+                    Added comments for questions and answers
+                    Formatted code and comments
+                    Pushed to GitHub
 */
 
 #include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printf("Science Quiz\nCreated By: Halle Carlson\n\n");
+    
+    printf("Please enter your name:");
+    string name;
+    cin >> name;
+    fseek(stdin, 0, SEEK_END);
+
+    printf("Hi, %s!\n", name.c_str());
+    //instructions here
+    printf("Good luck!\n\n");
+
+    int q1;
+    int q2;
+    int q3;
+
+    printf("Question 1:\n");
+    printf("%s, what, in layman's terms, is the role of vacuoles in animal cells?\n", name.c_str());
+    printf("\ta. adjust cell size\n");
+    printf("\tb. maintain water balance\n");    //correct answer
+    printf("\tc. waste management\n");
+    printf("\td. protein production\n");
+
+    //input and if statement
+
+    //question 2
+
+    //input and if statement
+
+    //question 3
+
+    //input and if statement
+
+    /*
+    Questions:
+    1.  What, in layman's terms, is the role of vacuoles in animal cells?
+        a. adjust cell size
+        b. maintain water balance
+        c. waste management                     //correct answer   
+        d. protein production
+
+    2. Which of the following is NOT a nitrogenous base present in RNA nucleotides?
+        a. adenine (A)
+        b. guanine (G)
+        c. cytosine (C)
+        d. thymine (T)                          //correct answer
+
+    3. What signifies that certain elements on the periodic table have the same valence electron configuration?
+        a. the elements are in the same period
+        b. the elements are in the same group   //correct answer
+        c. the elements are in the same subcategory
+        d. elements cannot have the same valence electron configurations
+    */
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
