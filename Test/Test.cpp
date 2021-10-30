b5c931ea06c4c9a8223ba4c1e5a8b4084b024a63
Halle Carlson <carlsonh@rsd407.org>
2021-10-29 20:26:06 -0700

    coded ASCII art bat

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,6 +1,8 @@
 // Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+// 10/29/21: coded ASCII art bat
+
 #include <iostream>
 #include <conio.h>
 #include <stdio.h>
@@ -10,10 +12,17 @@ using namespace std;
 
 int main()
 {
-	srand((unsigned)time(NULL));
-
-	float rf = (float)rand() / (float)RAND_MAX;				//[0.0, 5.0] random float within range
-	printf("Random float          ");                       //space to format output
-	printf("= %f\n", rf);
+	printf("______________            ________________\n");
+	printf(".              \\ |\\___/| /               .\n");
+	printf("  ~-.           \\/ o o \\/            .-~\n");
+	printf("     >          \\\\  W  //          <\n");
+	printf("    /            /~---~\\            \\\n");
+	printf("   /_           |       |           _\\\n");
+	printf("      ~-.       |       |       .-~\n");
+	printf("        ;        \\     /        ;\n");
+	printf("       /___      /\\   /\\      ___\\\n");
+	printf("            ~-. /  \\_/  \\ .-~\n");
+	printf("               V         V\n");
+	printf("Designed by Grathicks https://www.redbubble.com/i/art-board-print/Ascii-Bat-by-Grathicks/43006261.5E8EA");
 }
 
