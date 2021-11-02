b597aa81ed703fa116956d229f3036d0acb2e7e7
Halle Carlson <carlsonh@rsd407.org>
2021-11-02 08:17:36 -0700

    Started project, added code and comments

---
+++ choose your own adventure (if and switch)/choose your own adventure (if and switch).cpp
@@ -0,0 +1,32 @@
+// choose your own adventure (if and switch).cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+    Author:         Halle Carlson
+    Project Name:   Problem GS03-04 (Choose your own adventure using if and switch)
+    File Name:      choose your own adventure (if and switch).cpp
+    Purpose:        Write a short choose your own adventure story.Address the user by name and tell the story from their perspective.
+                    Give the user at least 3 places where the story will branch. The story will branch based on the user giving string
+                    input in at least 2 places and int input in at least 1 place. There must be at least 9 endings, each with an appropriate 
+                    ending. Either 2 levels of nesting with 3 choices at each level, or 3 levels of nesting with 2 choices at each level 
+                    (one choice would have 3 instead of 2).
+
+    Pseudocode:     
+
+    Maintenance Log:
+    Date: 11/2/21   Done:
+                    Created project
+                    Added comments
+                    Added code 
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+}
+
+
