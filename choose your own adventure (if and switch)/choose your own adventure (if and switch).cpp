0b296406cb3675594157c19c4e55ad4e7fe5ff67
Halle Carlson <carlsonh@rsd407.org>
2021-11-12 08:34:14 -0800

    Added code for narration and branching, added comments for clarity, formatted and edited code

--- choose your own adventure (if and switch)/choose your own adventure (if and switch).cpp
+++ choose your own adventure (if and switch)/choose your own adventure (if and switch).cpp
@@ -32,6 +32,13 @@
                     Added comments for clarity
                     Pushed to GitHub
 
+    Date: 11/12/21  Done: 
+                    Added code for narration and branching
+                    Added comments for clarity
+                    Formatted code
+                    Edited code
+                    Pushed to GitHub
+
 */
 
 #include <iostream>
@@ -49,6 +56,9 @@ int main()
     string name;
     cin >> name;
     fseek(stdin, 0, SEEK_END);
+
+    int weapon;
+    int door;
 
     printf("\n------------------------------------------------------------------------------------------------------------------------\n\n");
 
@@ -69,9 +79,7 @@ int main()
     printf("%s picked apart three possible choices.\n", name.c_str());
     printf("\ta. a light crossbow made of steel, with 3 sharp bolts\n");
     printf("\tb. a pair of polished shortswords with ornately-crafted hilts, suited for small hands to grip\n");
-    printf("\tc. a round shield, crafted with tough wood, metal rims, and leather straps on the back\n");
-
-    int weapon;
+    printf("\tc. a round shield, crafted with tough wood, metal rims, and leather straps on the back\n");    
 
     printf("What will %s take ?\n", name.c_str());
     printf("\ta. the strong crossbow\n");
@@ -84,9 +92,11 @@ int main()
     if (weapon == 'a' || weapon == 'A') //player chooses CROSSBOW
     {
         printf("------------------------------------------------------------------------------------------------------------------------\n\n");
-        printf("%s chose the strong crossbow\n", name.c_str());
+        printf("[%s chose the strong crossbow]\n", name.c_str());
 
-        printf("%s took the crossbow and the quiver with the bolts and slung them over their shoulder.\n", name.c_str());
+        printf("%s took the crossbow and the quiver with the bolts.\n", name.c_str());
+        printf("%s slung the quiver over their shoulder. The crossbow didn't have a strap, so %s figured they would need to carry it.\n\n", name.c_str());
+        _getch();
         printf("Suddenly, the weapons wall began to rumble!\n");
         printf("%s jumped backwards instinctively (perhaps moreso out of shock).\n", name.c_str());
         printf("%s landed in a puddle, drenching their pants and bare feet upon contact.\n", name.c_str());
@@ -99,9 +109,14 @@ int main()
     else if (weapon == 'b' || weapon== 'B') //player chooses BLADES
     {
         printf("------------------------------------------------------------------------------------------------------------------------\n\n");
-        printf("%s chose the shiny dual blades\n", name.c_str());
+        printf("[%s chose the shiny dual blades]\n", name.c_str());
 
-        printf("%s took (FINISH).\n", name.c_str());
+        printf("%s took the blades, which were hung next to a double holster made of worn leather.\n", name.c_str());
+        printf("%s took the holster, too.\n", name.c_str());
+        printf("%s equipped the holster, which could hold two small swords on the wearer's back.\n", name.c_str());
+        printf("%s reached behind their head and carefully slid one blade into each holster.\n", name.c_str());
+        printf("A perfect fit.\n\n");
+        _getch();
         printf("Suddenly, the weapons wall began to rumble!\n");
         printf("%s jumped backwards instinctively (perhaps moreso out of shock).\n", name.c_str());
         printf("%s landed in a puddle, drenching their pants and bare feet upon contact.\n", name.c_str());
@@ -114,9 +129,13 @@ int main()
     else if (weapon == 'c' || weapon == 'C') //player chooses SHIELD
     {
         printf("------------------------------------------------------------------------------------------------------------------------\n\n");
-        printf("%s chose the tough shield\n", name.c_str());
+        printf("[%s chose the tough shield]\n", name.c_str());
 
-        printf("%s (equiped the weapon, change language based on choice).\n", name.c_str());
+        printf("%s grabbed the shield with two hands.\n", name.c_str());
+        printf("It was much lighter than it looked.\n");
+        printf("%s slid their arm into the leather straps on the back.\n", name.c_str());
+        printf("%s tightened the straps enough so they could slip their arm in and out with ease but still have full control.\n\n", name.c_str());
+        _getch();
         printf("Suddenly, the weapons wall began to rumble!\n");
         printf("%s jumped backwards instinctively (perhaps moreso out of shock).\n", name.c_str());
         printf("%s landed in a puddle, drenching their pants and bare feet upon contact.\n", name.c_str());
@@ -127,14 +146,6 @@ int main()
     }
 
 
-    printf("%s (equiped the weapon, change language based on choice).\n", name.c_str());
-    printf("Suddenly, the weapons wall began to rumble!\n");
-    printf("%s jumped backwards instinctively (perhaps moreso out of shock).\n", name.c_str());
-    printf("%s landed in a puddle, drenching their pants and bare feet upon contact.\n", name.c_str());
-    printf("A trap wall fell from the cieling, making a terribly loud noise and blocking off the weapons wall!\n");
-    printf("%s took a moment to collect themself, but was startled back into reality when arrows flew out of small gaps on either wall.\n", name.c_str());
-    printf("In a surge of adrenaline, %s jumped to their feet, turned around, and eyed each door.\n", name.c_str());
-    printf("Each door was identical in appearence. %s shuddered at the thought of blindly flying into another trap.\n", name.c_str());
     
     int door;
 
@@ -149,31 +160,51 @@ int main()
     if (door == 'a' || door == 'A') //player chooses LEFT door
     {
         printf("------------------------------------------------------------------------------------------------------------------------\n\n");
+        printf("[%s chose the left door]\n\n", name.c_str());
 
-
-        
-        printf("------------------------------------------------------------------------------------------------------------------------\n\n");
         printf("%s pivoted and sprang towards the left door so quickly that they almost tripped and fell straight onto it.\n", name.c_str());
+
+        printf("%s gripped the door handle and pulled with all their might. (WEAPON-BASED TEXT)\n", name.c_str()); //OPENING THE DOOR TEXT DIFFERS BASED ON WEAPON
+        printf("The door was very heavy, but %s could open it far enough to slip through.\n", name.c_str());
+
+        //ROOM BRANCH
+        printf("[%s entered (ROOM)]\n", name.c_str()); //ROOM BASED ON DOOR, ENTERING DOOR TEXT BASED ON WEAPON (size & shape)
+
+        printf("The door led to a long, dark corridor.\n");
+        printf("Old-looking paintings lined the walls, framing menacing figures and scenes.\n");
+        printf("Small chandeliers hung from the cieling, providing light throughout the long passageway.\n");
+        printf("Most of the candles in the chandeliers were burnt out, but some still burned bright.\n");
+        printf("A fancy carped lined the floor, masking the sound of %s's footsteps.\n", name.c_str());
     }
 
     else if (door == 'b' || door == 'B') //player chooses RIGHT door
     {
         printf("------------------------------------------------------------------------------------------------------------------------\n\n");
-        
-        
-        
-        printf("------------------------------------------------------------------------------------------------------------------------\n\n");
+        printf("[%s chose the right door]\n\n", name.c_str());
+
         printf("%s pivoted and sprang towards the right door so quickly that they almost tripped and fell straight onto it.\n", name.c_str());
+
+        printf("%s gripped the door handle and pulled with all their might. (WEAPON-BASED TEXT)\n", name.c_str()); //OPENING THE DOOR TEXT DIFFERS BASED ON WEAPON
+        printf("The door was very heavy, but %s could open it far enough to slip through.\n", name.c_str());
+
+        printf("[%s entered (ROOM)]\n", name.c_str()); //ROOM BASED ON DOOR, ENTERING DOOR TEXT BASED ON WEAPON (size & shape)
+
+        //DIFFERENT ROOMS
     }
 
     else if (door == 'c' || door == 'C') //player chooses MIDDLE door
     {
         printf("------------------------------------------------------------------------------------------------------------------------\n\n");
-        
-        
-        
-        printf("------------------------------------------------------------------------------------------------------------------------\n\n");
+        printf("[%s chose the middle door]\n\n", name.c_str());
+
         printf("%s pivoted and sprang towards the middle door so quickly that they almost tripped and fell straight onto it.\n", name.c_str());
+
+        printf("%s gripped the door handle and pulled with all their might. (WEAPON-BASED TEXT)\n", name.c_str()); //OPENING THE DOOR TEXT DIFFERS BASED ON WEAPON
+        printf("The door was very heavy, but %s could open it far enough to slip through.\n", name.c_str());
+
+        printf("[%s entered (ROOM)]\n", name.c_str()); //ROOM BASED ON DOOR, ENTERING DOOR TEXT BASED ON WEAPON (size & shape)
+
+        //DIFFERENT ROOMS
     }
 
     printf("\n");
@@ -187,7 +218,7 @@ int main()
     printf("\n");
     printf("\n");
     printf("\n");
-    printf("\n");
+    printf("\n", name.c_str());
 
 
     printf("\n");
@@ -197,3 +228,16 @@ int main()
 }
 
 
+/*
+    [after weapons, before door]
+    printf("%s (equiped the weapon, change language based on choice).\n", name.c_str());
+    printf("Suddenly, the weapons wall began to rumble!\n");
+    printf("%s jumped backwards instinctively (perhaps moreso out of shock).\n", name.c_str());
+    printf("%s landed in a puddle, drenching their pants and bare feet upon contact.\n", name.c_str());
+    printf("A trap wall fell from the cieling, making a terribly loud noise and blocking off the weapons wall!\n");
+    printf("%s took a moment to collect themself, but was startled back into reality when arrows flew out of small gaps on either wall.\n", name.c_str());
+    printf("In a surge of adrenaline, %s jumped to their feet, turned around, and eyed each door.\n", name.c_str());
+    printf("Each door was identical in appearence. %s shuddered at the thought of blindly flying into another trap.\n", name.c_str());
+
+
+*/
