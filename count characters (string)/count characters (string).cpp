d2f9466406c8b490404b22eae752d832c76fc286
Halle Carlson <carlsonh@rsd407.org>
2021-11-19 08:19:50 -0800

    Added code and comments for program setup

--- count characters (string)/count characters (string).cpp
+++ count characters (string)/count characters (string).cpp
@@ -1,23 +1,31 @@
 // count characters (string).cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+    Author:         Halle Carlson
+    Project Name:   Problem GS04-03
+    File Name:      count characters (string).cpp
+    Purpose:        
 
+    Pseudocode:     
+
+    Maintenance Log:
+    Date: 11/15/21  Done:
+                    Created project  
+
+    Date: 11/19/21  Done:
+                    Added code and comments for setup
+                    Pushed to GitHub
+
+*/
 #include <iostream>
-
-//MAINTENANCE LOG
-    //11/15/21 created project
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+#include <windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    using namespace std;
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
