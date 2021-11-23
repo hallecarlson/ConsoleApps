47686cf5e63b7e88834699841507247231437d59
Halle Carlson <carlsonh@rsd407.org>
2021-11-23 08:34:11 -0800

    Added code, formatted output and code

--- count characters (string)/count characters (string).cpp
+++ count characters (string)/count characters (string).cpp
@@ -19,6 +19,13 @@
                     Minor changes
                     Pushed to GitHub
 
+    Date: 11/23/21  Done:
+                    Added code for output
+                    Added variables
+                    Added code for input and calculations
+                    Formatted code and output
+                    Pushed to GitHub
+
 */
 #include <iostream>
 #include <stdio.h>
@@ -29,9 +36,55 @@ using namespace std;
 
 int main()
 {
+    printf("------------------------------------------------------------------------------------------------------------------------\n\n");
+    printf("                                                Character Counter\n");
+    printf("                                            Created By: Halle Carlson\n\n");
+    printf("------------------------------------------------------------------------------------------------------------------------\n");
+
+    char retake;
     
-    
-  
+    do
+    {
+        int space;
+        int tab;
+        int newline;
+        int number;
+
+        printf("\nThis program gets user input and counts numbers, spaces, tabs, and newlines.\n");
+        printf("Please type something. When you are ready to input what you have typed, type a tilde (~) and hit the enter key.\n");
+        printf(">\n");
+        string input;
+        getline(cin, input, '~');
+        fseek(stdin, 0, SEEK_END);
+
+        for (int i = 0; i < input.length(); i++)
+        {
+            if (input[i] == ' ')
+            {
+                space++;
+            }
+            if (input[i] == '\t')
+            {
+                tab++;
+            }
+            if (input[i] == '\n')
+            {
+                newline++;
+            }
+            if (input[i] == '0' || input[i] == '1' || input[i] == '2' || input[i] == '3' || input[i] == '4' || input[i] == '5' || input[i] == '6' || input[i] == '7' || input[i] == '8' || input[i] == '9')
+            {
+                number++;
+            }
+        }
+
+        printf("\nSpaces:   %i\n", space);
+        printf("Tabs:     %i\n", tab);
+        printf("Newlines: %i\n", newline);
+        printf("Numbers:  %i\n\n", number);
+        printf("Input again ?\n (y for yes || any other key for no)");
+
+    } while (retake == 'y');
+
 
     return 0;
 }
