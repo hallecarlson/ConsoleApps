76b5a853df80fa8392609b99e56a878ceefe5ad6
Halle Carlson <carlsonh@rsd407.org>
2021-12-06 08:33:09 -0800

    created project, added code and comments

---
+++ RPGmainmenu/RPGmainmenu.cpp
@@ -0,0 +1,39 @@
+// RPGmainmenu.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+    Author:         Halle Carlson
+    Project Name:   Problem GS04-06 (RPG game loop and main menu)
+    File Name:      RPGmainmenu.cpp
+    Purpose:        Create a main menu (inside a loop) where you can choose to equip your character (and other things later) or enter the game.
+                    Equip the user with at least one item. You may choose to let the user have up to three items. There must be at least three 
+                    choices for each item.There should be a varuable that represents howmany items and variables to tell which items are chosen.
+                    (MORE)
+
+    Pseudocode:     
+
+
+    Notes:
+
+    Maintenance Log:
+    Date:   12/6/21     Done:
+                        Created project
+                        Added comments
+                        Added code
+                        Pushed to GitHub
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+   
+
+    return 0;
+}
+
+
