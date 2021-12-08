c91bbcb608199eb5fda67d8d934bd3f828d80194
Halle Carlson <carlsonh@rsd407.org>
2021-12-07 19:06:51 -0800

    created project, added comments, added basic starting code

--- word reverser/word reverser.cpp
+++ word reverser/word reverser.cpp
@@ -1,9 +1,15 @@
 // word reverser.cpp : This file contains the 'main' function. Program execution begins and ends there.
 /*
     Author:         Halle Carlson
-    Project Name:   Problem GS06-
-    File Name:      .cpp
-    Purpose:
+    Project Name:   Problem GS06-05
+    File Name:      word reverser.cpp
+    Purpose:        Write a program that reverses the characters in a string. While there is a function 
+                    that does this, using it does not count as writing a function. Your function should
+                    pass one or two strings to it. The first function has two arguments, the first argument
+                    is where the reversed string ends up and the second is where the original is passed.
+                    The second function gets passed the original string and returns the reversed string. 
+                    Make sure to keep the original string unchanged in the calling function (presumably 
+                    main()). Output the original and reversed strings in the calling function.
 
     Pseudocode:
 
