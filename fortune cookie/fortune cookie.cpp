2d307bf9853610f9705febea9d4cbe2c0a865ca7
Halle Carlson <carlsonh@rsd407.org>
2021-12-07 18:52:12 -0800

    created project, added comments, added basic starting code

---
+++ fortune cookie/fortune cookie.cpp
@@ -0,0 +1,29 @@
+// fortune cookie.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+    Author:         Halle Carlson
+    Project Name:   Problem GS06-
+    File Name:      .cpp
+    Purpose:
+
+    Pseudocode:
+
+
+    Notes:
+
+    Maintenance Log:
+    Date: 12/7/21       Done:
+                        Created project
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
---
+++ fraction reducer/fraction reducer.cpp
@@ -0,0 +1,38 @@
+// fraction reducer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+    Author:         Halle Carlson
+    Project Name:   Problem GS06-01
+    File Name:      fraction reducer.cpp
+    Purpose:        Write a program that reduces fractions using a function. The function will return
+                    an int and have two int arguments. The returned int will be the greatest common factor
+                    (GCF). 
+
+    Pseudocode:
+
+
+    Notes: 
+
+    Maintenance Log:
+    Date: 12/7/21       Done:
+                        Created project
+                        Added comments
+                        Added basic starting code
+                        Pushed to GitHub
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
+
+
+
+
