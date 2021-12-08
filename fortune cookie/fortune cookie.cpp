9efa6a71752a24ab9cd3519e9216f83adfebe0fd
Halle Carlson <carlsonh@rsd407.org>
2021-12-07 18:57:33 -0800

    created project, added comments, added basic starting code

--- fortune cookie/fortune cookie.cpp
+++ fortune cookie/fortune cookie.cpp
@@ -1,9 +1,15 @@
 // fortune cookie.cpp : This file contains the 'main' function. Program execution begins and ends there.
 /*
     Author:         Halle Carlson
-    Project Name:   Problem GS06-
-    File Name:      .cpp
-    Purpose:
+    Project Name:   Problem GS06-03
+    File Name:      fortune cookie.cpp
+    Purpose:        Write a program that is passed a number and possibly a string and gives the user a     
+                    fortune. You will decide which fortune, of ten possible, based on the number that is
+                    passed to the function. Then, the function will pass the fortune back to the calling
+                    function. Output the fortune in the calling function. You must loop the program (in 
+                    main()) so that the user can keep getting fortunes until opting to quit. As an extension,
+                    use a random number to determine the number you pass to the function instead of getting
+                    the number from the user.
 
     Pseudocode:
 
@@ -13,6 +19,9 @@
     Maintenance Log:
     Date: 12/7/21       Done:
                         Created project
+                        Added comments
+                        Added basic starting code
+                        Pushed to GitHub
 */
 
 #include <iostream>
