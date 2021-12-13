c951ccd900166d7726a2163cb4ffe92333baaf45
Halle Carlson <carlsonh@rsd407.org>
2021-12-13 08:32:22 -0800

    edited and compiled code, troubleshooted, got help from mr. miyoshi, re-compiled and tested coe, added pseudocode

--- fraction reducer/fraction reducer.cpp
+++ fraction reducer/fraction reducer.cpp
@@ -7,8 +7,11 @@
                     an int and have two int arguments. The returned int will be the greatest common factor
                     (GCF). 
 
-    Pseudocode:
-
+    Pseudocode:     GCF function
+                    Math for finding GCF
+                    Main function
+                    Inputs for numerator and denominator
+                    GCF and reduced fraction printed to screen
 
     Notes: 
 
@@ -38,6 +41,15 @@
                         Failed
                         Pushed to GitHub
 
+    Date: 12/13/21      Done:
+                        Pulled from GitHub
+                        Edited and compiled code
+                        Troubleshooted
+                        Got Help from Mr. Miyoshi
+                        Re-compiled and tested code
+                        Added pseudocode
+                        Pushed to gitHub
+
 */
 
 #include <iostream>
@@ -48,73 +60,40 @@ using namespace std;
 
 //int GCF();
 
-int GCF(int num, int denom, int gcf)
+int GCF(int num, int denom)
 {
-    //int i;
-
-    /*
-    if (num >= denom)
-    {
-        i = denom;
-    }
-    else if (denom > num)
-    {
-        i = num;
-    }
-    */
-
-    /*for (i >= 2; i--) //check this later
-    {
-        if (num % i == 0 && denom % i == 0) //change?
-        {
-            gcf = i;
-        }
-    }*/
-    int i;
-    if (num > denom)
-    {
-        i = num;
-    }
-    else if (denom > num)
-    {
-        i = denom;
-    }
-    else //same
-    {
-        i = num;
-    }
-    while (true)
+    num;
+    denom;
+    int gcf = 1;
+    for (int i = 2; i <= num; i++)
     {
         if (num % i == 0 && denom % i == 0)
         {
             gcf = i;
-            i--;
-            break;
         }
     }
+
     return gcf;
 }
 
 int main()
 {
     printf("Welcome to my Fraction Reducer program!\n");
-    printf("By: Halle Carlson\n\n");
+    printf("By: Halle Carlson\n\n"); 
 
     int num;
-    int denom;
-    int gcf;
-
     printf("Please enter a numerator\n");
     printf(">\n");
     scanf_s("%i", &num);
     fseek(stdin, 0, SEEK_END);
 
+    int denom;
     printf("Please enter a denominator\n");
     printf(">\n");
     scanf_s("%i", &denom);
     fseek(stdin, 0, SEEK_END);
 
-    GCF(num, denom, gcf); //function call
+    int gcf = GCF(num, denom); //function call
     printf("Your GCF (greatest common factor):\n");
     printf("%i\n", gcf);
 
@@ -122,14 +101,6 @@ int main()
     num = num / gcf; //error int division by 0
     denom = denom / gcf;
     printf("%i/%i\n", num, denom);
-
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
 
     return 0;
 }
