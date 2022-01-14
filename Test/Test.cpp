bcd9f3e9db39e6b6a31adee90f2d0a6ba0a8224a
Halle Carlson <carlsonh@rsd407.org>
2022-01-13 17:28:58 -0800

    practice code for looping competency test

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,7 +1,8 @@
 // Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-// 10/29/21: coded ASCII art bat
+// 10/29/21:	coded ASCII art bat
+// 1/13/22:		practice code for a looping competency test
 
 #include <iostream>
 #include <conio.h>
@@ -12,17 +13,40 @@ using namespace std;
 
 int main()
 {
-	printf("______________            ________________\n");
-	printf(".              \\ |\\___/| /               .\n");
-	printf("  ~-.           \\/ o o \\/            .-~\n");
-	printf("     >          \\\\  W  //          <\n");
-	printf("    /            /~---~\\            \\\n");
-	printf("   /_           |       |           _\\\n");
-	printf("      ~-.       |       |       .-~\n");
-	printf("        ;        \\     /        ;\n");
-	printf("       /___      /\\   /\\      ___\\\n");
-	printf("            ~-. /  \\_/  \\ .-~\n");
-	printf("               V         V\n");
-	printf("Designed by Grathicks https://www.redbubble.com/i/art-board-print/Ascii-Bat-by-Grathicks/43006261.5E8EA");
+	/*int num = 0;
+	int space = 1;
+
+	for (num = 15; num <= 125; num++)
+	{
+		if (num != 25 && num != 30 && num != 35)
+		{
+			printf("%i\t", num);
+		}
+		if (space % 5 == 0)
+		{
+			printf("\n");				
+		}		
+		space++;
+	}*/
+
+	/*int iNum = 0;
+	printf("Please input a positive number.\n>");
+	scanf_s("%i", &iNum);
+	fseek(stdin, 0, SEEK_END);
+	while (iNum <= 0)
+	{
+		printf("That is not a positive number. Try again.\n>");
+		scanf_s("%i", &iNum);
+		fseek(stdin, 0, SEEK_END);
+	}*/
+
+	/*int num = 100;
+	do
+	{
+		printf("%i\t", num);
+		num++;
+	} while (num <= 200);*/
+
+	return 0;
 }
 
