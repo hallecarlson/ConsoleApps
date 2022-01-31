321bfd022c8764a5c1035dc12d42a5213a77a669
Halle Carlson <carlsonh@rsd407.org>
2022-01-31 08:23:59 -0800

    created project

---
+++ valuesbyscalar/valuesbyscalar.cpp
@@ -0,0 +1,34 @@
+// valuesbyscalar.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+    Author:         Halle Carlson
+    Problem:        Write a program that declares an integer vector of 50 elements. (Use a loop to populate 
+                    the vector with consecutive or random numbers.) Then use the fact that scalar multiplication 
+                    of matrices (vectors) is simply the multiplication of each element in the matrix by the given 
+                    scalar. Let the user input a number (scalar) that will be used to multiply the matrix (vector). 
+                    Then multiply the matrix by that number. Output the new vector values to the screen.
+    Project name:   Problem GS10-01 (multiply vector values by scalar)
+    File name:      valuesbyscalar.cpp
+
+    Pseudocode:     
+
+    Maintenance Log:
+    Date: 1/31/22       Done:
+                        Created project
+                        Pushed to GitHub
+*/
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
