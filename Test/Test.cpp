7e454879a138983acf91acfafb8a510797ab2d40
Halle Carlson <carlsonh@rsd407.org>
2022-01-31 16:26:35 -0800

    Created a function for a for loop for printing numbers multiplied by a scalar, added code for user input of scalar value, got help from Ryan Carlson, tested code, added pseudocode, made minor changes to output format

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -13,15 +13,46 @@ using namespace std;
 #include <string>
 
 
-int main()
+void fxn(int hat, int shoes, int shirt)
 {
-	string sLName = "Carlson";
-	for (int i = sLName.length() - 1; i >= 0; i--)
+	return 0;
+}
+
+
+
+
+/*int compare(int num1, int num2, int num_lower)
+{
+	printf("Please input a number\n>");
+	scanf_s("%i", &num1);
+	fseek(stdin, 0, SEEK_END);
+		
+	printf("\nPlease input another number\n>");
+	scanf_s("%i", &num2);
+	fseek(stdin, 0, SEEK_END);
+
+	if (num1 < num2)
 	{
-		printf("%c", sLName[i]);
+		num_lower = num1;
+	}
+	else if (num2 < num1)
+	{
+		num_lower = num2;
 	}
 
+	return num_lower;
+}
+
+int main()
+{
+	int num1;
+	int num2;
+	int num_lower;
+	int output = compare(num1, num2, num_lower);
+
+	printf("Lower number: %i\n", num_lower);
+
 	_getch();
-	return 0;
+	return 0;*/
 }
 
--- valuesbyscalar/valuesbyscalar.cpp
+++ valuesbyscalar/valuesbyscalar.cpp
@@ -9,11 +9,28 @@
     Project name:   Problem GS10-01 (multiply vector values by scalar)
     File name:      valuesbyscalar.cpp
 
-    Pseudocode:     
+    Pseudocode:         Function for the for loop
+                            For loop for printing numbers multiplied by a scalar that is initially equal to 1, and is 
+                            then redefined by the user
+                        int main()
+                            Vector
+                            Function call
+                            Int for scalar
+                            User input for scalar
+                            Function call
 
     Maintenance Log:
     Date: 1/31/22       Done:
                         Created project
+                        Pushed to GitHub
+
+                        Pulled from GitHub
+                        Created a function for a for loop for printing numbers multiplied by a scalar
+                        Added code for user input of scalar value
+                        Got help from Ryan Carlson
+                        Tested code
+                        Added pseudocode
+                        Made minor changes to output format
                         Pushed to GitHub
 */
 
@@ -26,9 +43,30 @@
 #include <vector>
 using namespace std;
 
+void numbers(vector<int> nums, int scalar)
+{
+    for (int i = 0; i < 50; i++)
+    {
+        nums.push_back(i);
+        printf("%6i", nums.at(i)*scalar);
+    }
+
+    return;
+}
+
 int main()
 {
+    vector<int> nums;
+    numbers(nums, 1);
 
+    int scalar;
+    printf("\n\n\tPlease input a scalar to multiply these numbers by\n\t>");
+    scanf_s("%i", &scalar);
+    fseek(stdin, 0, SEEK_END);
+    printf("\n");
+
+    numbers(nums, scalar);
+    
 
     return 0;
 }
