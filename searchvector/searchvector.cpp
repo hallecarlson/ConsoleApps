36a32955b61e0ac514268fdaf73803146152a908
Halle Carlson <carlsonh@rsd407.org>
2022-02-26 20:08:16 -0800

    Got help from Ryan Carlson, Added code for project,  Tested code, Fixed minor errors,  Added pseudocode

--- searchvector/searchvector.cpp
+++ searchvector/searchvector.cpp
@@ -11,7 +11,19 @@
                     to output all the data in a line or let the user choose which columns (but at least the name 
                     and searched for data).
 
-    Pseudocode:
+    Pseudocode:         Struct with variables for data
+                        int main()
+                        Variables
+                        File opened
+                        File not found text
+                        File scanned
+                        Data pushed into a vector
+                        File closed
+                        User inputs the type of data they're searching
+                        User inputs the value they're searching
+                        Vector scanned for matches
+                        Data found and printed or not found and data not found message printed
+                        Option to input again
 
     Maintenance Log:
     Date: 2/23/22       Done:
@@ -25,6 +37,15 @@
                         Got help from Mr. Miyoshi
                         Pushed to GitHub
 
+                        Pulled from GitHub
+
+    Date: 2/26/22       Done:
+                        Got help from Ryan Carlson
+                        Added code for project
+                        Tested code
+                        Fixed minor errors
+                        Added pseudocode
+                        Pushed to GitHub
 */
 #include <iostream>
 #include <stdio.h>
@@ -43,11 +64,11 @@ struct Data
     string phone;
 };
 
-bool searching(Data d)
-
 int main()
 {
     vector<Data> data;
+    bool found = false;
+    char retake;
 
     ifstream in;
     ofstream out("vectorsearch.txt");
@@ -86,16 +107,128 @@ int main()
     out.close();
     in.close();
 
-    printf("Search a name, age, height, weight, or phone number.\n>");
+    do
+    {
+        string input;
+        printf("\nAre you searching a 1. name, 2. age, 3. height, 4. weight, or 5. phone number?\n>");
+        cin >> input;
+        fseek(stdin, 0, SEEK_END);
+
+        if (input == "1")
+        {
+            string input_name;
+            printf("Search a name\n>");
+            getline(cin, input_name);
+            fseek(stdin, 0, SEEK_END);
+            found = false;
+            printf("\nNames matching your search:\n");
+            for (int i = 0; i < data.size(); i++)
+            {
+                if (data[i].name == input_name)
+                {
+                    printf("%s\n", data[i].name.c_str());
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                printf("There are no people by that name.\n");
+            }
+        }
+
+        if (input == "2")
+        {
+            string input_age;
+            printf("Search an age\n>");
+            getline(cin, input_age);
+            fseek(stdin, 0, SEEK_END);
+            found = false;
+            printf("\nAges matching your search:\n");
+            for (int i = 0; i < data.size(); i++)
+            {
+                if (data[i].age == stoi(input_age))
+                {
+                    printf("%i\n", data[i].age);
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                printf("There are no people with that age.\n");
+            }
+        }
+
+        if (input == "3")
+        {
+            string input_height;
+            printf("Search a height (in inches)\n>");
+            getline(cin, input_height);
+            fseek(stdin, 0, SEEK_END);
+            found = false;
+            printf("\nAges matching your search:\n");
+            for (int i = 0; i < data.size(); i++)
+            {
+                if (data[i].height == stoi(input_height))
+                {
+                    printf("%i\n", data[i].height);
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                printf("There are no people with that height.\n");
+            }
+        }
+
+        if (input == "4")
+        {
+            string input_weight;
+            printf("Search a weight\n>");
+            getline(cin, input_weight);
+            fseek(stdin, 0, SEEK_END);
+            found = false;
+            printf("\nWeights matching your search:\n");
+            for (int i = 0; i < data.size(); i++)
+            {
+                if (data[i].age == stoi(input_weight))
+                {
+                    printf("%i\n", data[i].weight);
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                printf("There are no people with that weight.\n");
+            }
+        }
+
+        if (input == "5")
+        {
+            string input_phone;
+            printf("Search a phone number\n>");
+            getline(cin, input_phone);
+            fseek(stdin, 0, SEEK_END);
+            found = false;
+            printf("\nPhone numbers matching your search:\n");
+            for (int i = 0; i < data.size(); i++)
+            {
+                if (data[i].phone == input_phone)
+                {
+                    printf("%s\n", data[i].phone.c_str());
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                printf("There are no people with that phone number.\n");
+            }
+        }
+
+        printf("\nSearch again? (y for yes, else for no)\n>");
+        scanf_s("%c", &retake);
+        fseek(stdin, 0, SEEK_END);
+    } while (retake == 'y');
 
     _getch();
     return 0;
 }
-/*
-    Loretta,18,66,130,425.844.1231
-    Mike,16,68,145,425.844.1232
-    Charles,17,72,160,425.844.1233
-    Gracie,15,74,205,425.844.1234
-    Anna,19,73,170,425.844.1235
-    George,15,62,115,425.844.1236
-    */
