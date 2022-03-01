32214ef2fe229123b9465d75926f0a5a30aee0e5
Halle Carlson <carlsonh@rsd407.org>
2022-03-01 08:56:51 -0800

    added code for project

--- 1202_rectangleclass/rectangle.h
+++ 1202_rectangleclass/rectangle.h
@@ -1,1 +1,22 @@
 #pragma once
+#pragma once
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+class rectangle
+{
+public:
+    rectangle();
+    rectangle(float l, float w); //previously r1 and r2
+    rectangle(rectangle& R);
+    void setDimiensions(float length, float width); //previously rad1 and rad2
+    float getL();
+    float getW();
+    float perimeter();
+    float area();
+
+private:
+    float l;
+    float w;
+};
