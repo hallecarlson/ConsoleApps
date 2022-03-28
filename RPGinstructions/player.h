b37e066924de257bc1c4c2c2d04817184b4449cd
Halle Carlson <carlsonh@rsd407.org>
2022-03-28 08:35:48 -0700

    saved

--- RPGinstructions/player.h
+++ RPGinstructions/player.h
@@ -10,7 +10,7 @@ struct Player
     int hp, hpmin, hpmax, hphealed;
     int dp, dpmin, dpmax;
     int roll;
-    int lunchbox, potions;
+    int lunchbox, potions; 
     int item_weapon, item_misc;
 
     vector<bool> encountered_e;
