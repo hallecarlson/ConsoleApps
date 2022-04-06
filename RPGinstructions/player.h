cc85cbe09307abe2f1da8becda2fe4e62b4e3b20
Halle Carlson <carlsonh@rsd407.org>
2022-04-06 08:56:51 -0700

    saved

--- RPGinstructions/player.h
+++ RPGinstructions/player.h
@@ -5,16 +5,15 @@ using namespace std;
 
 struct Player
 {
-    string user; //formerly name
+    string user;
     int chara;
 
     int hp, hpmin, hpmax, hphealed;
     int dp, dpmin, dpmax; 
-    int roll;
+    int roll; 
     int lunchbox, potions;  
     int item_weapon, item_misc;
     int score;
-    //int room;
 
     vector<bool> encountered_e;
     vector<bool> encountered_m;
